76361c813cdf0ff508d42a8904a05a04fd15788e
balbano <balbano@gmail.com>
2014-06-23 20:10:09 -0500

    Added fps reporting.

--- PrairieAveLive_ExteriorMicNode.ino
+++ PrairieAveLive_ExteriorMicNode.ino
@@ -1,29 +1,45 @@
-/***********************************************
-Microphone Node, Arduino Fio, Electret mic, XBEE
-************************************************/
+/* 
+Prairie Avenue Live - Exterior Microphone Node
+==============================================
+
+The exterior mic node reads the volume data from each of the exterior
+mics and transmits it to the microcontroller running the LED panel.
+
+Parts
+-----
+
+- Arduino Fio: https://www.sparkfun.com/products/10116
+- Electret mics with auto gain: http://www.adafruit.com/products/1713
+- XBee Series 2: http://www.adafruit.com/products/968
+
+*/
 
 #include <XBee.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial softSerial(4, 5);
+// AUDIO
+const int numberOfMics = 2;
+const int micPins[] = {1, 2};
+uint8_t sampleMaxes[numberOfMics];
+const uint8_t sampleFloor = 100; // Use mic_check sketch to determine value.
+const uint8_t sampleCeiling = 190; // Use mic_check sketch to determine value.
 
-// SET UP XBEE
+// XBEE
 XBee xbee = XBee();
-
-uint8_t payload[] = {0, 0}; // payload, micPins, numberOfMics and sampleMaxes need to all correspond.
-
+uint8_t payload[numberOfMics]; // payload, micPins, numberOfMics and sampleMaxes need to all correspond.
 XBeeAddress64 addr64 = XBeeAddress64(0x0, 0x0); // coord 0x0013a200, 0x40ace8df
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 
-// SET UP AUDIO
-const int numberOfMics = 2; // payload, micPins, numberOfMics and sampleMaxes need to all correspond.
-const int micPins[] = {1, 2}; // payload, micPins, numberOfMics and sampleMaxes need to all correspond.
-uint8_t sampleMaxes[2]; // payload, micPins, numberOfMics and sampleMaxes need to all correspond.
+// TIMERS
+const int timeBetweenTransmissions = 40; // 25 FPS. (Panel runs at 12 FPS).
+unsigned long prevTransmissionTime;
 
-const uint8_t sampleFloor = 100; // Use mic_check sketch to determine value.
-const uint8_t sampleCeiling = 190; // Use mic_check sketch to determine value.
-const int sampleWindow = 40; // 25 FPS. (Panel runs at 12 FPS).
-unsigned long prevMillis;
+const int timeBetweenReports = 1000; // Report once per second.
+unsigned long prevReportTime;
+
+// REPORTING
+SoftwareSerial softSerial(4, 5); // RX, TX
+int numberOfTransmissions;
 
 void setup() 
 {
@@ -32,37 +48,42 @@ void setup()
   
   softSerial.begin(4800);
   
-  prevMillis = millis();
+  // Initialize the timers and counters;
+  prevTransmissionTime = millis();
+  prevReportTime = millis();
+  numberOfTransmissions = 0;
+  for (int i = 0; i < numberOfMics; i++){
+    sampleMaxes[i] = 0;
+  }
 }
 
 void loop() 
 {
-  for (int i = 0; i < numberOfMics; i++){
-    sampleMaxes[i] = 0;
-  }
-  
-  // Always take at least one set of samples.
-  do {
-    for (int i = 0; i < numberOfMics; i++) {
-      uint8_t rawSample = getSample(micPins[i]);
-      if (rawSample > sampleMaxes[i]) {
-        sampleMaxes[i] = rawSample;
-      }
+  for (int i = 0; i < numberOfMics; i++) {
+    uint8_t rawSample = getSample(micPins[i]);
+    if (rawSample > sampleMaxes[i]) {
+      sampleMaxes[i] = rawSample;
     }
-  } while (millis() - prevMillis < sampleWindow);
-  
-  prevMillis = millis();
-  
-  for (int i = 0; i < numberOfMics; i++){
-    uint8_t factoredSample = factorSample(sampleMaxes[i], sampleFloor, sampleCeiling);
-    payload[i] = factoredSample;
   }
   
-  softSerial.print(payload[0]);
-  softSerial.print(", ");
-  softSerial.println(payload[1]);
+  if (millis() - prevTransmissionTime > timeBetweenTransmissions) {
+    prevTransmissionTime = millis();
+    for (int i = 0; i < numberOfMics; i++){
+      uint8_t factoredSample = factorSample(sampleMaxes[i], sampleFloor, sampleCeiling);
+      payload[i] = factoredSample;
+    }
+    xbee.send(zbTx);
+    numberOfTransmissions++;
+    for (int i = 0; i < numberOfMics; i++){
+      sampleMaxes[i] = 0;
+    }
+  }
   
-  xbee.send(zbTx);
+  if (millis() - prevReportTime > timeBetweenReports) {
+    prevReportTime = millis();
+    softSerial.println(numberOfTransmissions);
+    numberOfTransmissions = 0;
+  }
 }
 
 uint8_t getSample(int pin) {
